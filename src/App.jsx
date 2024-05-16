f6cc9af23f8ce598bd3245184c16475921369a37
Aaron Ryu <fbcndah@gmail.com>
2024-05-16 15:02:05 +0900

    실습 6-2. 부모-자식 컴포넌트 : UnorderedListComponent

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,22 @@ function ListItemComponent({ children }) {
   return <li>{children}</li>
 }
 
-function UnorderedListComponent() {
-  const stringArray = ['Unordered List Item 1', 'Unordered List Item 2', 'Unordered List Item 3']
-  // String 의 배열을 받아서 <ListItemComponent> 의 새 배열을 만들어 반환하면되겠네
-  const reactArray = stringArray.map((each) => {
-    return <ListItemComponent>{each}</ListItemComponent>
-  })
-  console.log(stringArray)
-  console.log(reactArray)
+function UnorderedListComponent({ items }) {
   return (
     <ul>
-      {/* 구버전 */}
-      {/* 
-      <li>Unordered List Item 1</li>
-      <li>Unordered List Item 2</li>
-      <li>Unordered List Item 3</li>
-      */}
-      {/* 신버전 */}
-      {stringArray.map((each) => {
+      {items.map((each) => {
         return <ListItemComponent>{each}</ListItemComponent>
       })}
-      {/*
-      <ListItemComponent>Unordered List Item 1</ListItemComponent>
-      <ListItemComponent>Unordered List Item 2</ListItemComponent>
-      <ListItemComponent>Unordered List Item 3</ListItemComponent>
-      */}
     </ul>
   )
 }
 
 function App() {
+  const stringArray = ['Unordered List Item 1', 'Unordered List Item 2', 'Unordered List Item 3']
   return (
     <>
       <div>
-        {/* ul = Unordered List */}
-        <UnorderedListComponent />
-        {/* ol = Ordered List */}
-        <ol>
-          {/* li = List Item */}
-          <li>Ordered List Item 1</li>
-          <li>Ordered List Item 2</li>
-          <li>Ordered List Item 3</li>
-        </ol>
+        <UnorderedListComponent items={stringArray} />
       </div>
     </>
   )
