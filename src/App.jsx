d718585604128673ca2060de9c514e0878cd90f0
Aaron Ryu <fbcndah@gmail.com>
2024-05-16 10:55:16 +0900

    실습 3. 삼항 연산자를 활용한 조건부 컴포넌트 노출

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  return <></>;
+  const [age, setAge] = useState(0)
+  const [valid, setValid] = useState(false)
+
+  return (
+    <>
+      <input
+        type='number'
+        value={age}
+        onChange={(e) => {
+          // console.log(e.target.value)
+          const changed = e.target.value
+          // 1. 비축약 버전
+          // setAge(changed)
+          // if (changed >= 19) {
+          //   setValid(true)
+          // } else {
+          //   setValid(false)
+          // }
+          // 2. 축약 버전
+          setAge(changed)
+          setValid(changed >= 19)
+        }}
+      />
+      {/* 1. 단일 컴포넌트 내 표기할 값 변경 */}
+      {/* <div>{valid && '성년입니다.'}</div> */}
+      {/* 2. 상태값에 따라 다른 컴포넌트 노출 (삼항연산자) */}
+      {valid ? <div>성년입니다.</div> : <div style={{ color: 'red' }}>미성년입니다.</div>}
+    </>
+  )
 }
 
-export default App;
+export default App
