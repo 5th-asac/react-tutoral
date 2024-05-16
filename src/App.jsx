bb68d7e73412d54514cc92926832e1ff7313631a
Aaron Ryu <fbcndah@gmail.com>
2024-05-16 13:05:26 +0900

    실습 4. 자식 컴포넌트 2개 생성 - SetState 할용 방식 2가지

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,49 @@
 import { useState } from 'react'
 import './App.css'
 
+// 1. SetState 파라미터가 새로운 상태"값"
+function IncreaseButtonComponent({ count, setCount } /* Object Deconstructure 객체 비구조화 */) {
+  // console.log('props : ', props)
+  return (
+    <button
+      className='increase-button'
+      onClick={() => {
+        // props.setCount(props.count + 1)
+        setCount(count + 1)
+      }}
+    >
+      증가
+    </button>
+  )
+}
+
+// 2. SetState 파라미터가 새로운 상태를 생성하는 "함수"
+function DecreaseButtonComponent({ setCount }) {
+  return (
+    <button
+      className='decrease-button'
+      onClick={() => {
+        setCount((previousCount) => previousCount - 1)
+      }}
+    >
+      감소
+    </button>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
-  // React.Fragment : JSX 단위는 무조건 단일 태그로 감싸져야하기때문 <- JSX 식에는 부모 요소가 하나 있어야 합니다.ts(2657)
+  // * JSX 는 객체이고, 그 객체 내부에 props 라는 프로퍼티를 갖는다
+  const example = <div className='hello'>안녕하세요</div>
+  console.log('jsx example : ', example)
+
   return (
     <>
       <div style={{ marginBottom: 10 }}>{count}</div>
-      {/* tag 2가지 : button, input type="button" */}
       <div style={{ display: 'flex', flexDirection: 'row', gap: 10 }}>
-        <button
-          className='increase-button'
-          onClick={(e) => {
-            // SetState 사용법 2가지 : (1) 새로운 상태"값" (2) 새로운 상태값 생성 "함수"
-            // (1) setCount(count + 1)
-            // (2) setCount((previousCount) => {
-            //   return previousCount + 1
-            // })
-            setCount((previousCount) => previousCount + 1)
-          }}
-        >
-          증가
-        </button>
-        {/* onClick, onChange 같은것들의 파라미터는 Callback 함수(수행할것) */}
-        {/* click, change 같은것들은 이벤트라고 부르고 event 객체에 이벤트 관련된 값들을 다 담고있다. */}
-        <button
-          className='decrease-button'
-          onClick={(e) => {
-            setCount((previousCount) => previousCount - 1)
-          }}
-        >
-          감소
-        </button>
+        <IncreaseButtonComponent count={count} setCount={setCount} />
+        <DecreaseButtonComponent setCount={setCount} />
       </div>
     </>
   )
