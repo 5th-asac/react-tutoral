01bfe484c7ef9d3beb9b311340f2b743b52bca16
Aaron Ryu <fbcndah@gmail.com>
2024-05-16 17:05:26 +0900

    실습 6-4. Single Source of Truth 의도적 분리

--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,54 @@ import './App.css'
 
 function ListItemComponent({ item: person, children, setPeople }) {
   const [activated, setActivated] = useState(false)
+  const [changedDescription, setChangedDescription] = useState(person.desc)
+
   return (
     <li
-      onClick={() => {
+      onClick={(e) => {
         setActivated(!activated)
       }}
     >
       <div>{children}</div>
       <div>{person.age}</div>
       {activated ? (
-        <input
-          type='text'
-          value={person.desc}
-          onChange={(e) => {
-            const typed = e.target.value
-            // 원하는거 : 1개 요소에 대한 수정
-            // 파라미터 : 4개 요소로 구성된 배열
-            setPeople((previousPeople) => {
-              // const changedPerson = // 1개의 변한 Person
-              // const changedPeople = // 3개의 변하지 않은 Person + 1개의 변한 Person
-              // return []
-              //
-              // 우리가 천천히 차분하게 생각하면됩니다.
-              // 우리가 가지고 있는것은 무엇일까? 아래 3가지이다.
-              // - 입력받은 값 : typed
-              // - 내가 누구인지? 어떤 아이템인지 : person
-              // - 이전 상태(People 상태) : previousPeople
-              // const nextPeople = previousPeople.map((previousPerson) => {
-              //   if (person.name === previousPerson.name) {
-              //     // return {
-              //     //   name: previousPerson.name,
-              //     //   age: previousPerson.age,
-              //     //   desc: typed,
-              //     // }
-              //     return { ...previousPerson, desc: typed }
-              //   } else {
-              //     return previousPerson
-              //   }
-              // })
-              const nextPeople = previousPeople.map((previousPerson) =>
-                person.name === previousPerson.name
-                  ? { ...previousPerson, desc: typed }
-                  : previousPerson,
-              )
-              return nextPeople
-            })
-          }}
-        />
+        <>
+          <input
+            type='text'
+            value={changedDescription}
+            onChange={(e) => {
+              const typed = e.target.value
+              setChangedDescription(typed)
+            }}
+          />
+          <button
+            onClick={() => {
+              // 1. SetPeople 업데이트
+              // console.log('저장되었습니다.')
+              setPeople((previousPeople) => {
+                const nextPeople = previousPeople.map((previousPerson) => {
+                  if (previousPerson.name === person.name) {
+                    return { ...previousPerson, desc: changedDescription }
+                  } else {
+                    return previousPerson
+                  }
+                })
+                return nextPeople
+              })
+            }}
+          >
+            확인
+          </button>
+          <button
+            onClick={(e) => {
+              // 2. SetChangedDescription 를 통한 롤백
+              // console.log('원상복귀 되었습니다.')
+              setChangedDescription(person.desc)
+            }}
+          >
+            취소
+          </button>
+        </>
       ) : (
         <div>{person.desc}</div>
       )}
