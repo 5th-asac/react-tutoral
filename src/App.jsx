c3bf86ec5b55d1d3a1fa7e4ccf76c47300275952
Aaron Ryu <fbcndah@gmail.com>
2024-05-16 13:42:20 +0900

    실습 5. SetState 는 동기적이 아닌 비동기적으로 동작

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 
+const NON_ADAULT_FEE = 10000
+const ADAULT_FEE = 15000
+
 function App() {
   const [age, setAge] = useState(0)
   const [valid, setValid] = useState(false)
+  const [entrance, setEntrance] = useState(NON_ADAULT_FEE)
 
   return (
     <>
+      <div>{entrance}</div>
       <input
         type='number'
         value={age}
         onChange={(e) => {
-          // console.log(e.target.value)
           const changed = e.target.value
-          // 1. 비축약 버전
-          // setAge(changed)
-          // if (changed >= 19) {
-          //   setValid(true)
-          // } else {
-          //   setValid(false)
-          // }
-          // 2. 축약 버전
           setAge(changed)
           setValid(changed >= 19)
+          // 여러분들이 각자 한번 풀어보세요.
+          setEntrance(valid ? ADAULT_FEE : NON_ADAULT_FEE)
         }}
       />
-      {/* 1. 단일 컴포넌트 내 표기할 값 변경 */}
-      {/* <div>{valid && '성년입니다.'}</div> */}
-      {/* 2. 상태값에 따라 다른 컴포넌트 노출 (삼항연산자) */}
       {valid ? <div>성년입니다.</div> : <div style={{ color: 'red' }}>미성년입니다.</div>}
     </>
   )
