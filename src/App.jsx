c92a413dc62519680426ab19cbebd9b7b8cb7ad7
Aaron Ryu <fbcndah@gmail.com>
2024-05-20 13:45:39 +0900

    실습 13-1. Ref 통한 DOM 직접 조작 : color 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useRef } from 'react'
+import './App.css'
 
 function App() {
-  return <></>;
+  const textReference = useRef()
+  console.log('Re-rendered !')
+  return (
+    <>
+      <div ref={textReference}>Ref 를 통한 DOM 조작</div>
+      <button
+        onClick={() => {
+          if (textReference.current.style.color === 'white') {
+            textReference.current.style.color = 'red'
+          } else {
+            textReference.current.style.color = 'white'
+          }
+        }}
+      >
+        변경
+      </button>
+    </>
+  )
 }
 
-export default App;
+export default App
