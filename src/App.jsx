c800078f04e4c55f13ee1c78f22bfcd31646474b
Aaron Ryu <fbcndah@gmail.com>
2024-05-20 10:41:54 +0900

    실습 9-1. State 는 Rerendering 을 발생시킨다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0)
+  // Rerender 가 일어날때마다 표기
+  console.log(count)
+  return (
+    <>
+      {/* 1. 표기 View */}
+      <div>{count}</div>
+      {/* 2. 조작 Controller */}
+      <button onClick={() => setCount((prev) => prev + 1)}>증가</button>
+    </>
+  )
 }
 
-export default App;
+export default App
