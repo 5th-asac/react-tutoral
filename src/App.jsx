be0a6ba739980079cc684dce01ad1f81a42605c0
Aaron Ryu <fbcndah@gmail.com>
2024-05-20 15:50:45 +0900

    실습 14-4. 패스워드 에러 발생시 자식 컴포넌트 내 focus()

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import { useState, useRef } from 'react'
 import './App.css'
 
-function PasswordInputForm({ passwordReference, valid }) {
+// HOC : High Order Component
+function PrintErrorMessage({ valid, type }) {
+  if (valid) {
+    return <></>
+  }
+  if (type === 'long') {
+    return <div style={{ color: 'red' }}>비밀번호 너무 깁니다</div>
+  } else if (type === 'short') {
+    return <div style={{ color: 'red' }}>비밀번호 너무 짧습니다</div>
+  }
+}
+
+function PasswordInputForm({ passwordReference, valid /* { valid, type } */ }) {
   //
   function changeVisibleType(e) {
     if (passwordReference.current.type === 'password') {
@@ -18,14 +30,15 @@ function PasswordInputForm({ passwordReference, valid }) {
       <div>패스워드</div>
       <input ref={passwordReference} type='password' />
       <button onClick={changeVisibleType}>보임</button>
-      {valid ? <></> : <div>비밀번호 검증 에러 메세지</div>}
+      {/* {valid ? <></> : <div>비밀번호 검증 에러 메세지</div>} */}
+      <PrintErrorMessage valid={valid.valid} type={valid.type} />
     </>
   )
 }
 
 function App() {
   const passwordReference = useRef()
-  const [passwordValid, setPasswordValid] = useState(true)
+  const [passwordValid, setPasswordValid] = useState({ valid: true })
   return (
     <div>
       <>
@@ -39,9 +52,14 @@ function App() {
           console.log('- 입력받은 패스워드 : ', passwordReference.current.value)
           if (passwordReference.current.value.length < 8) {
             console.error('에러 : 패스워드가 너무 짧음')
-            setPasswordValid(false)
+            setPasswordValid({ valid: false, type: 'short' })
+            passwordReference.current.focus()
+          } else if (passwordReference.current.value.length > 12) {
+            console.error('에러 : 패스워드가 너무 김')
+            setPasswordValid({ valid: false, type: 'long' })
+            passwordReference.current.focus()
           } else {
-            setPasswordValid(true)
+            setPasswordValid({ valid: true })
           }
         }}
       >
