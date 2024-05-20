86eabf148097b9c16ffec87b9a272099f511ce22
Aaron Ryu <fbcndah@gmail.com>
2024-05-20 10:59:16 +0900

    실습 10-1. State 통한 인풋 폼은 입력시마다 리렌더가 발생

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  return <></>;
+  const [age, setAge] = useState(0)
+
+  // * Rerender 일어나는지 여부 판단을 위한 콘솔 로그
+  console.log('Re-rendered !')
+  return (
+    <>
+      <div>{age}</div>
+      <input type='number' value={age} onChange={(e) => setAge(e.target.value)} />
+    </>
+  )
 }
 
-export default App;
+export default App
