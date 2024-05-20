ba1743c5ed744296b575773b0f426a5d499af34e
Aaron Ryu <fbcndah@gmail.com>
2024-05-20 13:25:45 +0900

    실습 12. useEffect 도 마찬가지로 Ref 노출하지 않음

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useRef, useEffect } from 'react'
+import './App.css'
 
 function App() {
-  return <></>;
+  const countReference = useRef(0)
+
+  useEffect(() => {
+    console.log(countReference.current)
+    console.log(countReference.current.value)
+  }, [])
+
+  return (
+    <>
+      <input ref={countReference} type='number' />
+    </>
+  )
 }
 
-export default App;
+export default App
