d830a31fe4943632679db05dda9f1fca74f0d805
Aaron Ryu <fbcndah@gmail.com>
2024-05-20 11:07:41 +0900

    실습 10-2. Ref 통한 인풋 폼은 입력시마다 리렌더가 발생하지 않음

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
   const [age, setAge] = useState(0)
+  const ageReference = useRef(0)
 
   // * Rerender 일어나는지 여부 판단을 위한 콘솔 로그
   console.log('Re-rendered !')
   return (
     <>
-      <div>{age}</div>
-      <input type='number' value={age} onChange={(e) => setAge(e.target.value)} />
+      {/* 교훈 : ref.current 를 View 에 사용하는 멍청한짓은 하지말자 */}
+      {/* <div>{ageReference.current}</div> */}
+      <input
+        ref={ageReference}
+        type='number'
+        // value={ageReference.current}
+        // onChange={(e) => {
+        //   ageReference.current = e.target.value
+        // }}
+      />
+      <button
+        onClick={() => {
+          console.log('지금 값은 : ', ageReference.current.value)
+        }}
+      >
+        표기
+      </button>
     </>
   )
 }
