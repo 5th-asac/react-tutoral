a7d6769f83a017a270b7c72d5b19857d053411d1
Aaron Ryu <fbcndah@gmail.com>
2024-05-22 13:24:25 +0900

    실습 16. 함수형 컴포넌트에 ref 사용을 원할 시 forwardRef 필요 + 기명함수를 통한 디버깅이 유용하도록

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
-import { useState } from "react";
-import "./App.css";
+import { forwardRef, useRef, useState } from 'react'
+import './App.css'
+
+// '클래스 컴포넌트' : ref 를 쓰기위해서 한번 감싸기 위해 사용되는 컴포넌트 = Wrapper
+// 함수(형) 컴포넌트 : 실제 사용하는 컴포넌트
+const CustomInput = forwardRef(function WrappedCustomInput(
+  {
+    /* value, onChange */
+  },
+  ref,
+) {
+  if (true) {
+    throw Error('익명함수에러')
+  }
+  return <input type='number' ref={ref} /* value={value} onChange={onChange} */ />
+})
 
 function App() {
-  return <></>;
+  // const [number, setNumber] = useState(0)
+  const numberReference = useRef()
+
+  return (
+    <>
+      <CustomInput
+        ref={numberReference}
+        // value={number}
+        // onChange={(e) => {
+        //   setNumber(e.target.value)
+        // }}
+      />
+    </>
+  )
 }
 
-export default App;
+export default App
