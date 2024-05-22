f3bcd8b742bd609435d0a19b83c20317dc920762
Aaron Ryu <fbcndah@gmail.com>
2024-05-22 13:29:04 +0900

    실습 17. 단방향 바인딩과 양방향 바인딩의 차이 : 자식이 부모의 state 를 바로+직접 수정 가능한가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 
-function App() {
-  return <></>;
+// 단방향
+function CustomInput({ value, onChange }) {
+  // 1. value 상태를 통해 input 을 변경하기만 합니다.
+  return <input type='number' value={value} onChange={onChange} />
 }
 
-export default App;
+// 양방향
+function CustomInput({ value }) {
+  // 1. value 상태를 통해 input 을 변경하기도 하지만
+  // 2. input 이 변경되면 value 상태도 변경되는것입니다.
+  return <input type='number' value={value} />
+}
+
+function App() {
+  const [number, setNumber] = useState(0)
+  return (
+    <>
+      <CustomInput
+        value={number}
+        onChange={(e) => {
+          setNumber(e.target.value)
+        }}
+      />
+    </>
+  )
+}
+
+export default App
