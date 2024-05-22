6610a277def76cb1614c09afbe045cae006ed671
Aaron Ryu <fbcndah@gmail.com>
2024-05-22 15:17:00 +0900

    실습 18. useEffect 수행 시점은 paint 이후 + 3초 딜레이 후 상태변경에 따른 리렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useLayoutEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  return <></>;
+  const [surprise, setSurprise] = useState(false)
+
+  useEffect(
+    () => {
+      // setTimeout : 2개 파라미터
+      //  1. "특정 시간" 뒤 수행할 (콜백) 함수
+      //  2. "특정 시간" ms 단위 (3초 = 3000ms)
+      setTimeout(() => {
+        setSurprise(true)
+      }, 3000)
+    },
+    [
+      /* useEffect 안의 함수를 수행할지/말지를 판단하는 기준 : 의존성 배열 (Dependancy Array) */
+    ],
+  )
+
+  return (
+    <>
+      <div>{Math.random()}</div>
+      {surprise && <div>깜짝 선물 !</div>}
+    </>
+  )
 }
 
-export default App;
+export default App
