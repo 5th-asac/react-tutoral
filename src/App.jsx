a1e991143b11cbbbc8608fd210fd98141b2eaadf
Aaron Ryu <fbcndah@gmail.com>
2024-05-22 16:30:14 +0900

    실습 20. Props Drilling 발생 및 상태사용처 + 상태변경처 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 
-function App() {
-  return <></>;
+function TC({ count }) {
+  return (
+    <div className='component-box' style={{ padding: 10 }}>
+      Third Component : {count}
+    </div>
+  )
 }
 
-export default App;
+function SC({ count }) {
+  return (
+    <div className='component-box' style={{ padding: 10 }}>
+      Second Component
+      <TC count={count} />
+    </div>
+  )
+}
+
+function FC({ count }) {
+  return (
+    <div className='component-box' style={{ padding: 10 }}>
+      First Component
+      <SC count={count} />
+    </div>
+  )
+}
+
+function NonContextComponent({ onClick }) {
+  return (
+    <div className='component-box' style={{ padding: 10 }}>
+      NonContextComponent
+      <button onClick={onClick}>증가</button>
+    </div>
+  )
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 16 }}>
+      <FC count={count} />
+      <NonContextComponent onClick={() => setCount((prev) => prev + 1)} />
+    </div>
+  )
+}
+
+export default App
