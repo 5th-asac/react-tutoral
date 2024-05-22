4b58a5549c369fa0177a798337b132c3983b8344
Aaron Ryu <fbcndah@gmail.com>
2024-05-22 15:26:25 +0900

    실습 18-1. useEffect 의존성 배열 내 상태를 넣었을때

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,22 @@ import { useEffect, useLayoutEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [surprise, setSurprise] = useState(false)
+  const [count, setCount] = useState(0)
 
-  useEffect(
-    () => {
-      // setTimeout : 2개 파라미터
-      //  1. "특정 시간" 뒤 수행할 (콜백) 함수
-      //  2. "특정 시간" ms 단위 (3초 = 3000ms)
-      setTimeout(() => {
-        setSurprise(true)
-      }, 3000)
-    },
-    [
-      /* useEffect 안의 함수를 수행할지/말지를 판단하는 기준 : 의존성 배열 (Dependancy Array) */
-    ],
-  )
+  useEffect(() => {
+    console.log('changed ! : ', count)
+  }, [count/* useEffect 안의 함수를 수행할지/말지를 판단하는 기준 : 의존성 배열 (Dependancy Array) */])
 
   return (
     <>
-      <div>{Math.random()}</div>
-      {surprise && <div>깜짝 선물 !</div>}
+      <div>{count}</div>
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1)
+        }}
+      >
+        증가
+      </button>
     </>
   )
 }
