6c6ce9d97f5388bd912646c63679337bc2fd4e55
Aaron Ryu <fbcndah@gmail.com>
2024-05-25 14:25:53 +0900

    chore: set jsconfig.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  return <></>;
+  return <></>
 }
 
-export default App;
+export default App
